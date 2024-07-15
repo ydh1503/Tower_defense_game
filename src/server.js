6d8f5f36a4fbff699b4ac8852fd41eb04d292b7f
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-07-15 18:58:43 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,30 +1,9 @@
-// import net from 'net';
-// import initServer from './init/index.js';
-// import { config } from './config/config.js';
-// import { onConnection } from './events/onConnection.js';
-
-// // const PORT = 5555;
-
-// const server = net.createServer(onConnection);
-
-// initServer()
-//   .then(() => {
-//     server.listen(config.server.port, config.server.host, () => {
-//       console.log(`Echo server listening on port ${config.server.port}`);
-//       console.log(server.address());
-//     });
-//   })
-//   .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-//   });
-
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
-import initServer from './init/index.js';
 import { config } from './config/config.js';
+import accountsRouter from './routers/accounts.router.js';
 
 const app = express();
 const server = createServer(app);
@@ -32,10 +11,11 @@ const server = createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-initSocket(server); // 소켓 추가
-app.use(express.static('tower_defense_client'));
+app.use(express.static('tower_defense_client_online'));
+app.use('/', accountsRouter);
 
-initServer()
+// 소켓 추가
+initSocket(server)
   .then(() => {
     server.listen(config.server.port, async () => {
       console.log(`Server is running on port ${config.server.port}`);
@@ -45,3 +25,4 @@ initServer()
     console.error(e);
     process.exit(1);
   });
+
