ca71c7851cb2140e17679bf4cb2b5b1ee29a2a35
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-07-15 18:44:38 +0900

    Update and rename header.js to gameObjectTypes.js

---
+++ src/constants/gameObjectTypes.js
@@ -0,0 +1,15 @@
+const GAME_OBJECT_TYPES = {
+  OBJECT: {
+    PATH: 'path',
+    BASE: 'base',
+    SCORE: 'score',
+    LEVEL: 'level',
+    GOLD: 'gold',
+  },
+  OBJECT_ARRAY: {
+    TOWERS: 'towers',
+    MONSTERS: 'monsters',
+  },
+};
+
+export default GAME_OBJECT_TYPES;
