a2977b4540cc6129720eca82f7abf0cce3840143
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-07-16 12:27:10 +0900

    Update handlerMapping.js

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,11 +1,13 @@
 import { deadMonsterHandler, spawnMonsterHandler } from './game/monsterHandler.js';
 import startMatchingHandler from './startMatching.handler.js';
+import { purchaseTowerHandler } from './game/tower.handler.js';
 
 const handlerMappings = {
   // 1: 함수이름,
   2: startMatchingHandler,
   8: spawnMonsterHandler,
   16: deadMonsterHandler,
+  11: purchaseTowerHandler,
 };
 
 export default handlerMappings;
